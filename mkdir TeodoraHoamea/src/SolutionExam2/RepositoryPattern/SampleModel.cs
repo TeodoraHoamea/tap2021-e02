e687a242c4943b2e7387ce6f4f15d51c608c459b
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-04-16 08:44:04 +0300

    Added SampleModel

---
+++ mkdir TeodoraHoamea/src/SolutionExam2/RepositoryPattern/SampleModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RepositoryPattern
+{
+    public class SampleModel
+    {
+        public Guid Id { get; private set; }
+        public string DnaSequence { get; private set; }
+        public DateTime AnalysisTime { get; private set; }
+    }
+}
