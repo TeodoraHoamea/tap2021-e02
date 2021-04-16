8faa3bfe0a46e825afda7d147a0ddb48b819116b
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-04-16 08:20:53 +0300

    write IDataRepository interface

--- mkdir TeodoraHoamea/src/SolutionExam2/RepositoryPattern/IDataRepository.cs
+++ mkdir TeodoraHoamea/src/SolutionExam2/RepositoryPattern/IDataRepository.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace RepositoryPattern
 {
-    interface IDataRepository
+    public interface IDataRepository
     {
+        IQueryable<TEntity> Query<TEntity>() where TEntity : class;
+        void Delete<TEntity>(TEntity entity) where TEntity : class;
+        void Update<TEntity>(TEntity entity) where TEntity : class;
+        TEntity Insert<TEntity>(TEntity entity) where TEntity : class;
     }
 }
