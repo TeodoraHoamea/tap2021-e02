a15684672b0770559d9aa38e57234e8ac1280861
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-04-16 09:09:38 +0300

    added extension method

---
+++ mkdir TeodoraHoamea/src/SolutionExam2/IsValidSequence/IsValidDnaSequenceExtensionMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsValidSequence
+{
+    public static class IsValidDnaSequenceExtensionMethod
+    {
+        public static int ValidateDnaSequence(this String dna)
+        {
+            for(int i = 0; i<dna.Length; i++)
+            {
+                if (!dna[i].Equals('A') || !dna[i].Equals('C') || !dna[i].Equals('G') || !dna[i].Equals('T'))
+                    throw new InvalidOperationException("The DNA MUST conatain JUST characters A, C, D, T!!!!");
+            }
+            
+            return dna.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
