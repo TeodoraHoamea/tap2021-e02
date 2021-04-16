73b4bf3d66cf3d91a428948d0e42091890b1af3e
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-04-16 08:26:54 +0300

    write DataContext class

--- mkdir TeodoraHoamea/src/SolutionExam2/RepositoryPattern/DataContext.cs
+++ mkdir TeodoraHoamea/src/SolutionExam2/RepositoryPattern/DataContext.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace RepositoryPattern
 {
-    class DataContext
+    public class DataContext : DbContext, IUnitOfWork, IDataRepository
     {
+        public void Commit()
+        {
+            SaveChanges();
+        }
+
+        public void Delete<TEntity>(TEntity entity) where TEntity : class
+        {
+            Delete(entity);
+        }
+
+        public TEntity Insert<TEntity>(TEntity entity) where TEntity : class
+        {
+            return Insert(entity);
+        }
+
+        public IQueryable<TEntity> Query<TEntity>() where TEntity : class
+        {
+            return Set<TEntity>();
+        }
+
+        void IDataRepository.Update<TEntity>(TEntity entity)
+        {
+           Update(entity);
+        }
     }
 }
